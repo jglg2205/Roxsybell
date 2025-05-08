2b8f16a66729a96eb0dc9743982845f347ac7fc7
jglg2205 <josegregoriolopez22@gmail.com>
2025-05-08 00:40:06 -0500

    settings.py

--- joyeria/joyeria/settings.py
+++ joyeria/joyeria/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviroment.get('SECRET_KEY', default='roxsybell123')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='roxsybell123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
