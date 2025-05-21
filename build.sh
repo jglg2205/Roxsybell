ee7786453a520637136d755edb1886ddeca650c9
jglg2205 <josegregoriolopez22@gmail.com>
2025-05-20 22:38:31 -0500

    b

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r joyeria/requirements.txt
 
-python manage.py collectstatic --no-input
-python pmanage.py migrate
+python joyeria/manage.py collectstatic --no-input
+python joyeria/manage.py migrate
